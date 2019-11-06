98d451008c2e61fa538aa37bbd42d183f2aec9cd
edz <zhangjianxun@qiumiapp.com>
2019-11-06 16:42:40 +0800

    update 0.7.2

--- AppFrame/Utils/Marco/FoundationGlobalHeader.h
+++ AppFrame/Utils/Marco/FoundationGlobalHeader.h
@@ -83,44 +83,37 @@ return nil; \
 return self; \
 }
 
-#ifndef    weakify
-#if __has_feature(objc_arc)
-
-#define weakify( x ) \\
-_Pragma("clang diagnostic push") \\
-_Pragma("clang diagnostic ignored \\"-Wshadow\\"") \\
-autoreleasepool{} __weak __typeof__(x) __weak_##x##__ = x; \\
-_Pragma("clang diagnostic pop")
-
-#else
-
-#define weakify( x ) \\
-_Pragma("clang diagnostic push") \\
-_Pragma("clang diagnostic ignored \\"-Wshadow\\"") \\
-autoreleasepool{} __block __typeof__(x) __block_##x##__ = x; \\
-_Pragma("clang diagnostic pop")
-
-#endif
+#ifndef weakify
+    #if DEBUG
+        #if __has_feature(objc_arc)
+            #define weakify(object) autoreleasepool{} __weak __typeof__(object) weak##_##object = object;
+        #else
+            #define weakify(object) autoreleasepool{} __block __typeof__(object) block##_##object = object;
+        #endif
+    #else
+        #if __has_feature(objc_arc)
+            #define weakify(object) try{} @finally{} {} __weak __typeof__(object) weak##_##object = object;
+        #else
+            #define weakify(object) try{} @finally{} {} __block __typeof__(object) block##_##object = object;
+        #endif
+    #endif
 #endif
 
-#ifndef    strongify
-#if __has_feature(objc_arc)
 
-#define strongify( x ) \\
-_Pragma("clang diagnostic push") \\
-_Pragma("clang diagnostic ignored \\"-Wshadow\\"") \\
-try{} @finally{} __typeof__(x) x = __weak_##x##__; \\
-_Pragma("clang diagnostic pop")
-
-#else
-
-#define strongify( x ) \\
-_Pragma("clang diagnostic push") \\
-_Pragma("clang diagnostic ignored \\"-Wshadow\\"") \\
-try{} @finally{} __typeof__(x) x = __block_##x##__; \\
-_Pragma("clang diagnostic pop")
-
-#endif
+#ifndef strongify
+    #if DEBUG
+        #if __has_feature(objc_arc)
+            #define strongify(object) autoreleasepool{} __typeof__(object) object = weak##_##object;
+        #else
+            #define strongify(object) autoreleasepool{} __typeof__(object) object = block##_##object;
+        #endif
+    #else
+        #if __has_feature(objc_arc)
+            #define strongify(object) try{} @finally{} __typeof__(object) object = weak##_##object;
+        #else
+            #define strongify(object) try{} @finally{} __typeof__(object) object = block##_##object;
+        #endif
+    #endif
 #endif
 
 
--- AppFrame/ViewController.m
+++ AppFrame/ViewController.m
@@ -24,6 +24,7 @@
     
     UIImage *image = [NSBundle getPodImageWith:nil fileName:@"NavigationBack" type:@"png"];
     self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithTarget:self action:@selector(clickLeftBarButtonItem) image:image itemSpaces:QMBarItemSpaceMake(0, 0)];
+    @weakify(self);
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
