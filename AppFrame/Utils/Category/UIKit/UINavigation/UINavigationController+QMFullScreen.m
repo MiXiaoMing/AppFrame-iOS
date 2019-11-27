15f2a535b9e6078ca530ab906929ca07363c084f
edz <zhangjianxun@qiumiapp.com>
2019-11-27 20:25:58 +0800

    update 0.8.0

--- AppFrame/Utils/Category/UIKit/UINavigation/UINavigationController+QMFullScreen.m
+++ AppFrame/Utils/Category/UIKit/UINavigation/UINavigationController+QMFullScreen.m
@@ -24,30 +24,30 @@
 
 @implementation UINavigationController (QMFullScreen)
 
-+ (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        [self swizzleClassInstanceMethodWithOriginSel:@selector(viewDidLoad) swizzleSel:@selector(QM_viewDidLoad)];
-    });
-}
-
--(void)QM_viewDidLoad {
-    //接替系统滑动返回手势
-    NSArray *internalTargets = [self.interactivePopGestureRecognizer valueForKey:@"targets"];
-    id internalTarget = [internalTargets.firstObject valueForKey:@"target"];
-    SEL handler = NSSelectorFromString(@"handleNavigationTransition:");
-    
-    UIPanGestureRecognizer * fullScreenGesture = [[UIPanGestureRecognizer alloc]initWithTarget:internalTarget action:handler];
-    fullScreenGesture.delegate = self;
-    fullScreenGesture.maximumNumberOfTouches = 1;
-    
-    UIView *targetView = self.interactivePopGestureRecognizer.view;
-    [targetView addGestureRecognizer:fullScreenGesture];
-    
-    [self.interactivePopGestureRecognizer setEnabled:NO];
-    
-    [self QM_viewDidLoad];
-}
+//+ (void)load {
+//    static dispatch_once_t onceToken;
+//    dispatch_once(&onceToken, ^{
+//        [self swizzleClassInstanceMethodWithOriginSel:@selector(viewDidLoad) swizzleSel:@selector(QM_viewDidLoad)];
+//    });
+//}
+//
+//-(void)QM_viewDidLoad {
+//    //接替系统滑动返回手势
+//    NSArray *internalTargets = [self.interactivePopGestureRecognizer valueForKey:@"targets"];
+//    id internalTarget = [internalTargets.firstObject valueForKey:@"target"];
+//    SEL handler = NSSelectorFromString(@"handleNavigationTransition:");
+//
+//    UIPanGestureRecognizer * fullScreenGesture = [[UIPanGestureRecognizer alloc]initWithTarget:internalTarget action:handler];
+//    fullScreenGesture.delegate = self;
+//    fullScreenGesture.maximumNumberOfTouches = 1;
+//
+//    UIView *targetView = self.interactivePopGestureRecognizer.view;
+//    [targetView addGestureRecognizer:fullScreenGesture];
+//
+//    [self.interactivePopGestureRecognizer setEnabled:NO];
+//
+//    [self QM_viewDidLoad];
+//}
 
 /**
  全屏滑动返回判断
@@ -55,25 +55,25 @@
  @param gestureRecognizer 手势
  @return 是否响应
  */
-- (BOOL)gestureRecognizerShouldBegin:(UIPanGestureRecognizer *)gestureRecognizer {
-    if (self.topViewController.QM_disableInteractivePop) {
-        return NO;
-    }
-    
-    if ([gestureRecognizer translationInView:gestureRecognizer.view].x <= 0) {
-        return NO;
-    }
-    
-    if ([[self valueForKey:@"_isTransitioning"] boolValue]) {
-        return NO;
-    }
-    
-    return (self.childViewControllers.count != 1);
-}
-
--(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-    return (otherGestureRecognizer.state == UIGestureRecognizerStateBegan && [otherGestureRecognizer.view isKindOfClass:NSClassFromString(@"UILayoutContainerView")]);
-}
+//- (BOOL)gestureRecognizerShouldBegin:(UIPanGestureRecognizer *)gestureRecognizer {
+//    if (self.topViewController.QM_disableInteractivePop) {
+//        return NO;
+//    }
+//
+//    if ([gestureRecognizer translationInView:gestureRecognizer.view].x <= 0) {
+//        return NO;
+//    }
+//
+//    if ([[self valueForKey:@"_isTransitioning"] boolValue]) {
+//        return NO;
+//    }
+//
+//    return (self.childViewControllers.count != 1);
+//}
+//
+//-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+//    return (otherGestureRecognizer.state == UIGestureRecognizerStateBegan && [otherGestureRecognizer.view isKindOfClass:NSClassFromString(@"UILayoutContainerView")]);
+//}
 
 
 
