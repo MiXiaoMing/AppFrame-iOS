daf9f4b1934ecd87674452357087376e577ccf81
wsure1990 <412530879@qq.com>
2019-12-07 10:18:48 +0800

    导航栏返回按钮 字体 颜色

--- AppFrame/Framework/Base/View/QMBaseNavigationController.m
+++ AppFrame/Framework/Base/View/QMBaseNavigationController.m
@@ -9,6 +9,9 @@
 #import "QMBaseNavigationController.h"
 #import "UIImage+QMGenerate.h"
 #import "UIKitGlobalHeader.h"
+#import "UIBarButtonItem+QMCreat.h"
+#import "NSBundle+QMPod.h"
+
 @interface QMBaseNavigationController ()
 
 @end
@@ -21,11 +24,30 @@
     UIImage *barImg = [UIImage squareImageWithColor:kColorWith_RGB_Hex(0xe73736) targetSize:CGSizeMake(KSCREEN_WIDTH, 88)];
     [self.navigationBar setBackgroundImage:barImg forBarMetrics:UIBarMetricsDefault];
     [self.navigationBar setShadowImage:[UIImage new]];
+    [self.navigationBar setTitleTextAttributes:@{@"NSFontAttributeName" : kFont(18)]}];
 }
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
     UIViewController* topVC = self.topViewController;
     return [topVC preferredStatusBarStyle];
+}
+
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    // 非根控制器
+    if (self.childViewControllers.count > 0) {
+        // 设置返回按钮
+        viewController.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithTarget:self action:@selector(back) image:[NSBundle getPodImageWith:@"AppFrame" fileName:@"top_icon_back" type:@"png"] itemSpaces:QMBarItemSpaceMake(15, 15)];
+        
+        // 隐藏TabBar
+        [viewController setHidesBottomBarWhenPushed:YES];
+    }
+    
+    // 真正跳转
+    [super pushViewController:viewController animated:animated];
+}
+
+- (void)back {
+    [self popViewControllerAnimated:YES];
 }
 
 @end
